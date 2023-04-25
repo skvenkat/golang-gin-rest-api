4e9af444cf70d4d429c48576fedb704afe93ea5f
S.K.Venkat <1156760+skvenkat@users.noreply.github.com>
2023-04-25 22:33:07 +0100

    GO_GIN_REST: enabled basic auth by adding Singup method in the handlers module

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -28,3 +28,42 @@ func (ga GoApp) Home() gin.HandlerFunc {
 		ctx.JSON(http.StatusOK, gin.H{"resp": "Welcome to gin-gonic web app"})
 	}
 }
+
+func (ga *GoApp) SignUp() gin.HandlerFunc {
+    return func (ctx *gin.Context) {
+        var user *model.User
+        if err := ctx.ShouldBindJSON(&user); err != nil {
+            _ = ctx.AbortWithError(http.StatusBadRequest, gin.Error{Err: err})
+        }
+        user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+        user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+        user.Password, _ = encrypt.Hash(user.Password)
+        if err := ga.App.Validate.Struct(&user); err != nil {
+            if _, ok := err.(*validator.InvalidValidationError); !ok {
+                _ = ctx.AbortWithError(http.StatusBadRequest, gin.Error{Err: err})
+                ga.App.InfoLogger.Println(err)
+                return
+            }
+        }
+
+        ok, status, err := ga.DB.InsertUser(user)
+        if err != nil {
+            _ = ctx.AbortWithError(http.StatusInternalServerError, errors.New("error while adding new user"))
+            ctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+            return
+        }
+        if !ok {
+            _ = ctx.AbortWithError(http.StatusInternalServerError, err)
+            return
+        }
+
+        switch status {
+        case 1:
+            ctx.JSON(http.StatusOK, gin.H{"message": "Registered Successfully"})
+            return
+        case 2:
+            ctx.JSON(http.StatusFound, gin.H{"message": "Existing Account, Go to the Login page"})
+            return
+        }
+    }
+}
